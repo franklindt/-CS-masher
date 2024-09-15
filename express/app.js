3522ae4395c3ee31a24c91f104be08e10c0c39ac
Andrew Schineller <34293975+schineaj23@users.noreply.github.com>
2024-09-14 22:11:29 -0400

    a

--- express/app.js
+++ express/app.js
@@ -4,15 +4,25 @@ const cors = require('cors');
 const helmet = require('helmet');
 const compression = require('compression');
 const bodyParser = require('body-parser');
-
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const dotenv = require('dotenv').config();
 
 app.use(cors());
 app.use(helmet());
 app.use(compression());
 app.disable('x-powered-by');
 
-
 const port = 3000
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@meowmeow.4kfr5.mongodb.net/?retryWrites=true&w=majority&appName=meowmeow`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -22,4 +32,16 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
