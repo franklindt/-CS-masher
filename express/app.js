30ea58223d2fd8c74c1eee93450c4a56e85ddd0d
franklindt <fr4nklinszhang@gmail.com>
2024-09-14 21:56:44 -0400

    Scooby

--- express/app.js
+++ express/app.js
@@ -1,5 +1,23 @@
 const express = require('express')
 const app = express()
+const leetcode = new LeetCode()
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
+const bodyParser = require('body-parser');
+
+
+
+const corsOptions = require('./config/cors.config');
+const connectMongo = require('./config/mongo.config');
+
+app.use(cors(corsOptions));
+app.use(helmet());
+app.use(compression());
+app.use(bodyParser());
+app.disable('x-powered-by');
+
+
 const port = 3000
 
 app.get('/', (req, res) => {
